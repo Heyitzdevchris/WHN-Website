78759ada386e71d1540fb972933636b51254e49e
Chris Ibarra <chrisaaron184@gmail.com>
2024-03-20 10:44:37 -0700

    Cleaned Up Code & Added className to h1

--- src/pages/schedule-page/Schedule.jsx
+++ src/pages/schedule-page/Schedule.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Schedule() {
   return (
-    <h1>The Schedule Page Is Currently Under Construction...</h1>
+    <h1 className='schedule-content'>
+        The Schedule Page Is Currently Under Construction...
+    </h1>
   );
 };
