ecae0e6db594549f4916e34a21c1691b522ab62f
Chris Ibarra <chrisaaron184@gmail.com>
2024-03-21 06:28:50 -0700

    Removed Files

--- src/pages/schedule-page/Schedule.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Schedule() {
-  return (
-    <h1 className='schedule-content'>
-        The Schedule Page Is Currently Under Construction...
-    </h1>
-  );
-};
