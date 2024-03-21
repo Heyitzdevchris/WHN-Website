8eb29688a56dfacacdfee43568f856dd6dd70390
Chris Ibarra <chrisaaron184@gmail.com>
2024-03-21 13:18:56 -0700

    Created a Component for the Homepage Feature Implementation

---
+++ src/home/components/HqHomepageForm.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './HqHomepageForm.css';
+
+export default function HqHomepageForm() {
+  return (
+    <div className="hq-rental-software-integration-homepage">
+        <iframe 
+            src="https://whn-enterprises-llc.whnrentals.com/public/car-rental/reservations/embed?brand=ao0x2buz-ndyt-o4xl-pmcd-cclemcfyfp19&new=true&reservation_page=https%3A%2F%2Fwhnrentals.com%2Freserve"
+            title="Homepage Form"
+            width="1200"
+            height="800" 
+            frameBorder="0"
+            crossOrigin="annonymous"
+        ></iframe>
+        <script 
+            src="https://whn-enterprises-llc.whnrentals.com/public/car-rental/integrations/assets/integrator"
+            type="text/javascript"
+        ></script>
+    </div>
+  );
+};
