2ebb725d7c0a3e32c041fe52045075adf8737bc4
Chris Ibarra <chrisaaron184@gmail.com>
2024-03-25 16:17:49 -0700

    Typo Fix on className's & Parameters

--- src/pages/about-page/components/AboutAdvertisement.jsx
+++ src/pages/about-page/components/AboutAdvertisement.jsx
@@ -4,7 +4,7 @@ import AboutWhnBackground from '../../../images/about-us-advertisement.jpeg';
 
 export default function AboutAdvertisement() {
   return (
-    <section className="hero" style={{backgroundImage: `url(${AboutWhnBackground})`}}>
+    <section className="about-hero" style={{backgroundImage: `url(${AboutWhnBackground})`}}>
       <div className="about-us-content">
         <h1>About WHN Enterprise</h1>
         <p>Take a look under the hood</p>
--- src/pages/reservation-page/Reservation.jsx
+++ src/pages/reservation-page/Reservation.jsx
@@ -5,12 +5,12 @@ export default function Reservation() {
   return (
     <div className="hq-rental-software-integration">
         <iframe
-            src="https://whn-enterprises-llc.whnrentals.com/public/car-rental/reservations/recover-last-booking?brand_uuid=ao0x2buz-ndyt-o4xl-pmcd-cclemcfyfp19"
-            title="Reservation Engine"
-            width="100%"
-            height="800"
-            frameBorder="0"
-            crossOrigin="annonymous"
+          src="https://whn-enterprises-llc.whnrentals.com/public/car-rental/reservations/recover-last-booking?brand_uuid=ao0x2buz-ndyt-o4xl-pmcd-cclemcfyfp19"
+          title="Reservation Engine"
+          width="100%"
+          height="800"
+          frameBorder="0"
+          crossOrigin="anonymous"
         ></iframe>
         <script 
             src="https://whn-enterprises-llc.whnrentals.com/public/car-rental/integrations/assets/integrator"
