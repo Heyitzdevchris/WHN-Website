355041adadbf525a076379b0b1fdd97d834705d3
Chris Ibarra <chrisaaron184@gmail.com>
2024-03-25 16:18:13 -0700

    Typo Fix on Parameter

--- src/home/components/HqHomepageForm.jsx
+++ src/home/components/HqHomepageForm.jsx
@@ -5,12 +5,12 @@ export default function HqHomepageForm() {
   return (
     <div className="hq-rental-software-integration-homepage">
         <iframe 
-            src="https://whn-enterprises-llc.whnrentals.com/public/car-rental/reservations/embed?brand=ao0x2buz-ndyt-o4xl-pmcd-cclemcfyfp19&new=true&reservation_page=https%3A%2F%2Fwhnrentals.com%2Freserve"
-            title="Homepage Form"
-            width="1200"
-            height="800" 
-            frameBorder="0"
-            crossOrigin="annonymous"
+          src="https://whn-enterprises-llc.whnrentals.com/public/car-rental/reservations/embed?brand=ao0x2buz-ndyt-o4xl-pmcd-cclemcfyfp19&new=true&reservation_page=https%3A%2F%2Fwhnrentals.com%2Freservation"
+          title="Homepage Form"
+          width="1200"
+          height="800" 
+          frameBorder="0"
+          crossOrigin="anonymous"
         ></iframe>
         <script 
             src="https://whn-enterprises-llc.whnrentals.com/public/car-rental/integrations/assets/integrator"
