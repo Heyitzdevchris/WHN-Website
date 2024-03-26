cc205eece6929902a329083605e56fde9bb5cb73
Chris Ibarra <chrisaaron184@gmail.com>
2024-03-26 14:35:40 -0700

    Increased the Height of Reservation Widget

--- src/pages/reservation-page/Reservation.jsx
+++ src/pages/reservation-page/Reservation.jsx
@@ -8,7 +8,7 @@ export default function Reservation() {
           src="https://whn-enterprises-llc.whnrentals.com/public/car-rental/reservations/recover-last-booking?brand_uuid=ao0x2buz-ndyt-o4xl-pmcd-cclemcfyfp19"
           title="Reservation Engine"
           width="100%"
-          height="800"
+          height="1000"
           frameBorder="0"
           crossOrigin="anonymous"
         ></iframe>
