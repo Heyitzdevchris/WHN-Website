9c1132b9dfe2f8041506098486228e8674e477a9
Chris Ibarra <chrisaaron184@gmail.com>
2024-03-29 00:52:37 -0700

    Moved ViewFleet to Fleet Page & Added Initial ViewFleet Content

--- src/home/components/ViewFleet.jsx
+++ src/home/components/ViewFleet.jsx
@@ -1,69 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import ScionImg from '../../images/scion.jpg';
-import HeliosImg from '../../images/helioshomepage.jpg';
-import StormTrooperImg from '../../images/stormtrooper.jpg';
-import WhiteNoiseImg from '../../images/whitenoise.jpg';
-import TesiImg from '../../images/tesi.jpg';
 import './ViewFleet.css';
 
 
-const ViewFleet = () => {
+export default function ViewFleet() {
   return (
-    <div className="fleet-container">
-        <h1>View The Fleet</h1>
-        <div className="card__container">
-            <article className="card__article">
-                <img src={ScionImg} alt="White Scion TC" className="card__img" />
-
-                <div className="card__data">
-                    <span className="card__description">Meet "Old Faithful"</span>
-                    <h2 className="card__title">2012 Scion TC</h2>
-                    <a href="/fleet" className="card__button">Read More</a>
-                </div>
-            </article>
-            <article className="card__article">
-                <img src={HeliosImg} alt="Red Tesla Sedan" className="card__img" />
-
-                <div className="card__data">
-                    <span className="card__description">Meet "Helios"</span>
-                    <h2 className="card__title">2023 Tesla Model Y</h2>
-                    <a href="/fleet" className="card__button">Read More</a>
-                </div>
-            </article>
-            <article className="card__article">
-                <img src={StormTrooperImg} alt="White Telsa Sedan" className="card__img" />
-
-                <div className="card__data">
-                    <span className="card__description">Meet "White Noise"</span>
-                    <h2 className="card__title">2023 Tesla Model Y</h2>
-                    <a href="/fleet" className="card__button">Read More</a>
-                </div>
-            </article>
-            <article className="card__article">
-                <img src={WhiteNoiseImg} alt="White Tesla SUV" className="card__img" />
-
-                <div className="card__data">
-                    <span className="card__description">Meet "Stormtrooper"</span>
-                    <h2 className="card__title">2020 Tesla Model 3</h2>
-                    <a href="/fleet" className="card__button">Read More</a>
-                </div>
-            </article>
-            <article className="card__article">
-                <img src={TesiImg} alt="White Tesla White Rimmed SUV" className="card__img" />
-
-                <div className="card__data">
-                    <span className="card__description">Meet "Tesi"</span>
-                    <h2 className="card__title">2021 Tesla Model Y</h2>
-                    <a href="/fleet" className="card__button">Read More</a>
-                </div>
-            </article>
+    <div className="view-fleet-container">
+        <div className='view-fleet-heading'>
+            <h1>Find Your Drive</h1>
+            <span>Car rentals from WHN Enterprise</span>
         </div>
-        <Link to="/fleet" className='view-fleet-button'>
-            View Fleet
-        </Link>
+        <div className="view-fleet-btn-section">
+            <Link to='/fleet'>
+                <button className="view-fleet-btn">
+                    Browse Fleet
+                </button>
+            </Link>
+        </div>
     </div>
   );
 };
 
-export default ViewFleet;
