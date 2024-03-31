754348be5eeb0eaf980660c9562b1ebb156199b6
Chris Ibarra <chrisaaron184@gmail.com>
2024-03-31 14:42:08 -0700

    Added Initial Content for Ad Section to Fleet Page

--- src/pages/fleet-page/Fleet.jsx
+++ src/pages/fleet-page/Fleet.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
+import FleetPageAdImg from '../../images/fleetPageAd.jpg';
 import ScionImg from '../../images/scion.jpg';
 import HeliosImg from '../../images/helioshomepage.jpg';
 import StormTrooperImg from '../../images/stormtrooper.jpg';
@@ -10,51 +11,58 @@ import './Fleet.css';
 
 export default function Fleet() {
   return (
-    <div className="fleet-container">
-        {/* <h1>View The Fleet</h1> */}
-        <div className="card__container">
-            <article className="card__article">
-                <img src={ScionImg} alt="White Scion TC" className="card__img" />
-                <div className="card__data">
-                    <span className="card__description">Meet "Old Faithful"</span>
-                    <h2 className="card__title">2012 Scion TC</h2>
-                    <a href="/fleet" className="card__button">Read More</a>
-                </div>
-            </article>
-            <article className="card__article">
-                <img src={HeliosImg} alt="Red Tesla Sedan" className="card__img" />
-                <div className="card__data">
-                    <span className="card__description">Meet "Helios"</span>
-                    <h2 className="card__title">2023 Tesla Model Y</h2>
-                    <a href="/fleet" className="card__button">Read More</a>
-                </div>
-            </article>
-            <article className="card__article">
-                <img src={StormTrooperImg} alt="White Telsa Sedan" className="card__img" />
-                <div className="card__data">
-                    <span className="card__description">Meet "White Noise"</span>
-                    <h2 className="card__title">2023 Tesla Model Y</h2>
-                    <a href="/fleet" className="card__button">Read More</a>
-                </div>
-            </article>
-            <article className="card__article">
-                <img src={WhiteNoiseImg} alt="White Tesla SUV" className="card__img" />
-                <div className="card__data">
-                    <span className="card__description">Meet "Stormtrooper"</span>
-                    <h2 className="card__title">2020 Tesla Model 3</h2>
-                    <a href="/fleet" className="card__button">Read More</a>
-                </div>
-            </article>
-            <article className="card__article">
-                <img src={TesiImg} alt="White Tesla White Rimmed SUV" className="card__img" />
-                <div className="card__data">
-                    <span className="card__description">Meet "Tesi"</span>
-                    <h2 className="card__title">2021 Tesla Model Y</h2>
-                    <a href="/fleet" className="card__button">Read More</a>
-                </div>
-            </article>
+    <section>
+        <div className='fleet-hero' style={{backgroundImage: `url(${FleetPageAdImg})`}}>
+            <div className="fleet-ad-content">
+                <h1 className="fleet-ad-heading">Experience WHN's Fleet Selection</h1>
+                <p className="fleet-ad-text">Find Your Perfect Drive</p>
+            </div>
         </div>
-    </div>
+        <div className="fleet-container">
+            <div className="card__container">
+                <article className="card__article">
+                    <img src={ScionImg} alt="White Scion TC" className="card__img" />
+                    <div className="card__data">
+                        <span className="card__description">Meet "Old Faithful"</span>
+                        <h2 className="card__title">2012 Scion TC</h2>
+                        <a href="/fleet" className="card__button">Read More</a>
+                    </div>
+                </article>
+                <article className="card__article">
+                    <img src={HeliosImg} alt="Red Tesla Sedan" className="card__img" />
+                    <div className="card__data">
+                        <span className="card__description">Meet "Helios"</span>
+                        <h2 className="card__title">2023 Tesla Model Y</h2>
+                        <a href="/fleet" className="card__button">Read More</a>
+                    </div>
+                </article>
+                <article className="card__article">
+                    <img src={StormTrooperImg} alt="White Telsa Sedan" className="card__img" />
+                    <div className="card__data">
+                        <span className="card__description">Meet "White Noise"</span>
+                        <h2 className="card__title">2023 Tesla Model Y</h2>
+                        <a href="/fleet" className="card__button">Read More</a>
+                    </div>
+                </article>
+                <article className="card__article">
+                    <img src={WhiteNoiseImg} alt="White Tesla SUV" className="card__img" />
+                    <div className="card__data">
+                        <span className="card__description">Meet "Stormtrooper"</span>
+                        <h2 className="card__title">2020 Tesla Model 3</h2>
+                        <a href="/fleet" className="card__button">Read More</a>
+                    </div>
+                </article>
+                <article className="card__article">
+                    <img src={TesiImg} alt="White Tesla White Rimmed SUV" className="card__img" />
+                    <div className="card__data">
+                        <span className="card__description">Meet "Tesi"</span>
+                        <h2 className="card__title">2021 Tesla Model Y</h2>
+                        <a href="/fleet" className="card__button">Read More</a>
+                    </div>
+                </article>
+            </div>
+        </div>
+    </section>
   );
 };
 
