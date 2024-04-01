49e42a985fa2b19e245041efdcd9988c66e27a33
Chris Ibarra <chrisaaron184@gmail.com>
2024-04-01 02:51:16 -0700

    Added Media Query for Tablet in Landscape View for Ad Content

--- src/home/components/Advertisement.jsx
+++ src/home/components/Advertisement.jsx
@@ -6,7 +6,7 @@ import HqHomepageForm from './HqHomepageForm';
 const Advertisement = () => {
     return (
         <section className="hero" style={{backgroundImage: `url(${BackgroundImage})`}}>
-            <div className="advertisement-content">
+            <div className="advertisement-content" style={{paddingTop: '300px'}}>
                 <h1>The WHN Experience</h1>
                 <p>Excellence By Design</p>
                 <HqHomepageForm />
