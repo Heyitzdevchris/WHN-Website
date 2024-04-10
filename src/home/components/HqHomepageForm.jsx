722d4794b18b4ce16b97abf7e093cc1d75f6d7d7
Chris Ibarra <chrisaaron184@gmail.com>
2024-04-10 09:30:19 -0700

    Increased Widget Height

--- src/home/components/HqHomepageForm.jsx
+++ src/home/components/HqHomepageForm.jsx
@@ -8,7 +8,7 @@ export default function HqHomepageForm() {
           src="https://whn-enterprises-llc.whnrentals.com/public/car-rental/reservations/embed?brand=ao0x2buz-ndyt-o4xl-pmcd-cclemcfyfp19&new=true&reservation_page=https%3A%2F%2Fwhnrentals.com%2Freservation"
           title="Homepage Form"
           width="1200"
-          height="500" 
+          height="800" 
           frameBorder="0"
           crossOrigin="anonymous"
         ></iframe>
