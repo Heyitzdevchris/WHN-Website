9a5572a0705aa3cd0657c9d2d3e073bc66fefeae
christopher ibarra <chrisaaron184@gmail.com>
2024-04-14 16:44:12 -0700

    Added Initial Desgin for View Fleet Section on Homepage for all Viewports

--- src/home/components/ViewFleet.jsx
+++ src/home/components/ViewFleet.jsx
@@ -1,23 +1,54 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import TeslaDriving from '../../videos/tesla-driving-homepage.mp4';
 import './ViewFleet.css';
 
-
 export default function ViewFleet() {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const video = videoRef.current;
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.5 // 50% of the video needs to be in the viewport
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          video.play();
+        } else {
+          video.pause();
+        }
+      });
+    }, options);
+
+    observer.observe(video);
+
+    // Cleanup observer
+    return () => {
+      observer.unobserve(video);
+    };
+  }, []);
+
   return (
-    <div className="view-fleet-container">
-        <div className='view-fleet-heading'>
-            <h1>Find Your Drive</h1>
-            <p>Car rentals from WHN Enterprise</p>
-        </div>
-        <div className="view-fleet-btn-section">
+    <div className="viewFleetContainer">
+        <div className='viewFleetContent'>
+            <h1 className='viewFleetTitle'>Explore Your Drive</h1>
+            <p className='viewFleetDescription'>Car rentals from WHN Enterprise</p>
             <Link to='/fleet'>
-                <button className="view-fleet-btn">
+                <button className="viewFleetBtn">
                     Browse Fleet
                 </button>
             </Link>
         </div>
+        <video ref={videoRef} className='teslaVideo' muted autoPlay loop>
+            <source src={TeslaDriving} type="video/mp4" />
+            Your browser does not support the video tag.
+        </video>
     </div>
   );
-};
+}
+
 
